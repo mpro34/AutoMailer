97826a6dd3944361bb422faca8d01ed3f821d2ec
Chris Whiting <chriswhiting@Chriss-MacBook-Pro.local>
2017-12-31 17:23:07 -0800

    Added more routes, logout, and passing of the userid from mongo to client

---
+++ models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+//Model what the collection will contain. i.e. add a name property, etc.
+const userSchema = new Schema({
+  googleId: String
+});
+
+//Create new collection called users in MongoDB!
+mongoose.model('users', userSchema);
--- services/passport.js
+++ services/passport.js
@@ -1,8 +1,22 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id); //user.id is shorthand for user._id.$oid in mongo
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 //console.developers.google.com - Google+ API Google oath API
+//TODO Add Facebook and Linkedin Strategy for Login as well!
 passport.use(
   new GoogleStrategy({
       clientID: keys.googleClientID,
@@ -10,9 +24,19 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile', profile);
+      User.findOne({ googleId: profile.id })
+          .then((existingUser) => {
+            if (existingUser) {
+              //We already have a record with the given profile ID
+              done(null, existingUser);
+            } else {
+              //We don't have a record with this profile ID, create new record.
+              new User({ googleId: profile.id })
+                .save()
+                .then(user => done(null, user));
+
+            }
+          })
     }
   )
 );
