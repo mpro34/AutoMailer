3f798c4421e7fe7e48a19008a7b3ce0430cef72d
Chris Whiting <chriswhiting@Chriss-MacBook-Pro.local>
2018-01-01 20:48:59 -0800

    Connect React side with node server side

--- services/passport.js
+++ services/passport.js
@@ -24,20 +24,15 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id })
-          .then((existingUser) => {
-            if (existingUser) {
-              //We already have a record with the given profile ID
-              done(null, existingUser);
-            } else {
-              //We don't have a record with this profile ID, create new record.
-              new User({ googleId: profile.id })
-                .save()
-                .then(user => done(null, user));
-
-            }
-          })
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id })
+      if (existingUser) {
+      //We already have a record with the given profile ID
+        return done(null, existingUser);
+      }
+    //We don't have a record with this profile ID, create new record.
+      const user = await new User({ googleId: profile.id }).save()
+      done(null, user);
     }
   )
 );
