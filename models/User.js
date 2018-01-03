bc7d7739fffa3d58ca90d7e92653c7e66e0220f4
Chris Whiting <chriswhiting@Chriss-MacBook-Pro.local>
2018-01-02 16:30:27 -0800

    Added payment with the Stripe API, linked react with express

--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ const { Schema } = mongoose;
 
 //Model what the collection will contain. i.e. add a name property, etc.
 const userSchema = new Schema({
-  googleId: String
+  googleId: String,
+  credits: { type: Number, default: 0 }
 });
 
 //Create new collection called users in MongoDB!
