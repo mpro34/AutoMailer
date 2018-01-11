eae66ecbd82abe443a00f03cd3eb0cf8dc54c3dd
Chris Whiting <chris.whiting@panasonic.aero>
2018-01-11 07:53:32 -0800

    Added reduxForm components and wired up survey forms

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,10 @@
+//SurveyField contains logic to render a single label and trext inpit
+import React from 'react';
+
+export default ({ input }) => {
+  return (
+    <div>
+      <input {...input} />
+    </div>
+  );
+};
