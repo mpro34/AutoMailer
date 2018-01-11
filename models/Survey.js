591b20aecc528ee9c7e169076714f02c0bd2aff7
Chris Whiting <chriswhiting@Chriss-MacBook-Pro.local>
2018-01-10 20:36:02 -0800

    Added sendgrid email webhook, a new Mailer class, and Survey/Recipient mongoose models

---
+++ models/Survey.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const RecipientSchema = require('./Recipient');
+
+const surveySchema = new Schema({
+  title: String,
+  body: String,
+  subject: String,
+  recipients: [RecipientSchema],  //An array containing a list of strings
+  yes: { type: Number, default: 0 },
+  no: { type: Number, default: 0 },
+  _user: { type: Schema.Types.ObjectId, ref: 'User' }, //Each survey gets assigned a specific user
+  dateSent: Date,
+  lastResponded: Date
+});
+
+mongoose.model('surveys', surveySchema);
